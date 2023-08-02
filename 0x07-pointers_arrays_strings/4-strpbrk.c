d634c12f6d1a08ca12c29fe354a7fc47859405ff
zakariiia <zakariamoukrim57@gmail.com>
2023-08-01 19:07:06 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ * @s:first occurrence in the string
+ * @accept: matches one of the bytes, or @NULL if no such byte
+ * Return: a pointer to the byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int m;
+		while (*s)
+		{
+			for (m = 0; accept[m]; m++)
+			{
+				if (*s == accept[m])
+					return (s);
+			}
+			s++;
+		}
+	return (NULL);
+}
