5985ecd72ce57bd46f3c1d413b39d822a7586f48
zakariiia <zakariamoukrim57@gmail.com>
2023-08-24 16:21:09 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
 /**
- * struct list_s - singly linked list 
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure 0x12-singly_linked_lists
- * for alx africa project C17-2023 by zakaria moukrim
+ * Description: singly linked list node structure
+ * for alx  project
  */
 typedef struct list_s
 {
@@ -19,6 +20,8 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
